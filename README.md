6ab1cc3693e983dc14b8d1a5afe1c527a6d0f146
TayKaiJun <tehohno@gmail.com>
2023-07-26 01:14:23 +0800

    Initial commit
    Following tutorial from https://www.youtube.com/watch?v=vZtm1wuA2yc&ab_channel=Indently

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # SSA_telebot
- Telegram Bot for SSA
+Telegram Bot for SSA
+
+## User Guide
+> ⚠️ When adding bot to groups, bot must be promoted to admin in group setting
+ 
+## Developer Details
+Currently hosted on https://www.pythonanywhere.com 
+
+- Bot name: SSA Ah Gong \
+- Bot username: uclassa_telebot \
+- Bot description: As SSA's Ah Gong, I shall watch over SSA - tele channels and make people's lifes easier.
+- <details>
+    <summary>HTTP API</summary>
+      6429496443:AAEe_RoDvPPrSbdD2q23bRK8fNhDSkgiKJQ
+  </details>
+- Image from Ah Kong Durian (https://www.ahkongdurian.com/):
+  <img src="./img/ahgong.png">
+  <!-- Please check for copyright conflicts! -->
---
+++ src/main.py
@@ -0,0 +1,70 @@
+from typing import final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: final = "6429496443:AAEe_RoDvPPrSbdD2q23bRK8fNhDSkgiKJQ"
+BOT_USERNAME: final = "@uclassa_telebot"
+
+# Commands
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello children ")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("What do you want")
+
+# Responses
+# AI will go in here later on to process inputs
+
+def handle_response(text: str) -> str:
+    processed_text: str = text.lower()
+
+    if "hello" in processed_text:
+        return "Selamat Pagi"
+    
+    if "singapore" in processed_text:
+        return "Majulah Singapura"
+    
+    return "Ah Gong don't understand"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f"User ({update.message.chat.id}) in {message_type} says: {text}")
+    
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+          
+    else:
+        response: str = handle_response(text)
+        
+    print("Bot: ", response)
+    await update.message.reply_text(response)
+    
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+    
+    
+# Main
+
+if __name__ == "__main__":
+    app = Application.builder().token(TOKEN).build()
+    
+    # Commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # Error
+    app.add_error_handler(error)
+    
+    # Polls the bot for updates
+    print("Bot is running...")
+    app.run_polling(poll_interval=3)
